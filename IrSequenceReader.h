471dd8ca297083154cd9a7702f2a3c2ad1903479
Bengt Martensson <barf@bengt-martensson.de>
2015-10-26 17:50:16 +0100

    New class IrSequenceReader.

---
+++ IrSequenceReader.h
@@ -0,0 +1,43 @@
+#ifndef IRSEQUENCEREADER_H
+#define	IRSEQUENCEREADER_H
+
+#include "IrSequence.h"
+#include "IrReader.h"
+
+/**
+ * This class packs an IrSequence into a dummy, immutable IrReader.
+ */
+class IrSequenceReader : public IrReader {
+private:
+    IrSequence irSequence;
+
+public:
+    IrSequenceReader() : irSequence() {
+    };
+
+    IrSequenceReader(const IrSequenceReader& orig) : irSequence(orig.irSequence) {
+    };
+
+    IrSequenceReader(const IrSequence irSequence_) : irSequence(irSequence_) {
+    };
+
+    virtual ~IrSequenceReader() {
+    };
+
+    void receive() {
+    };
+
+    boolean isReady() const {
+        return true;
+    };
+
+    unsigned int getDataLength() const {
+        return irSequence.getLength();
+    };
+
+    microseconds_t getDuration(unsigned int index) const {
+        return irSequence.getDurations()[index];
+    };
+};
+
+#endif	/* IRSEQUENCEREADER_H */

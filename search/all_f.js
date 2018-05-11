2f84cda2d71c56eb583e331ba4d10e3dfed2e7d7
Bengt Martensson <barf@bengt-martensson.de>
2018-05-11 19:33:01 +0200

    Updated for Version-1.0.2.

--- search/all_f.js
+++ search/all_f.js
@@ -15,6 +15,7 @@ var searchData=
   ['timervaluetonanoseconds',['timerValueToNanoSeconds',['../classIrWidget.html#a8b09fff5460ce629ebd40f363d2450fd',1,'IrWidget']]],
   ['timesince',['timeSince',['../IrReceiverPoll_8cpp.html#a835436c679a41d525f0be6272d708a20',1,'IrReceiverPoll.cpp']]],
   ['toirsequence',['toIrSequence',['../classIrReader.html#abc437af677c211cf7961d5f5e810ef21',1,'IrReader']]],
+  ['toprontohex',['toProntoHex',['../classPronto.html#a95abb2bc5ac634a351a34b5341b0867c',1,'Pronto::toProntoHex(const IrSignal &amp;irSignal)'],['../classPronto.html#a89e7fb52a4028517be7f46adfa913a05',1,'Pronto::toProntoHex(const microseconds_t *data, size_t length, frequency_t frequency=IrSignal::defaultFrequency)'],['../classPronto.html#a38eb76e0185bdf594fcfec94d1acd869',1,'Pronto::toProntoHex(const IrSequence &amp;irSequence, frequency_t frequency=IrSignal::defaultFrequency)']]],
   ['trydecode',['tryDecode',['../classNec1Decoder.html#a23aba73fc1229950ca7ea5e1581673fd',1,'Nec1Decoder::tryDecode()'],['../classRc5Decoder.html#a1ba9c0ad97d7bbec861baca097071515',1,'Rc5Decoder::tryDecode()']]],
   ['type',['Type',['../classMultiDecoder.html#a6b119d114e11f2fd74a74f0c8524a2a6',1,'MultiDecoder']]]
 ];

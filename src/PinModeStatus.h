4fab08d2fc493c15b8f2b2ab7243dafeb69260de
eudoxos <eu@doxos.eu>
2020-06-22 12:42:58 +0200

    add STM32 arch to PinMode #ifdefs (compiles now)

--- src/PinModeStatus.h
+++ src/PinModeStatus.h
@@ -23,7 +23,7 @@ this program. If not, see http://www.gnu.org/licenses/.
 
 #if !defined(ARDUINO) || defined(ARDUINO_ARCH_AVR) || defined(ARDUINO_ARCH_SAMD) \
  || defined(ARDUINO_ARCH_SAM) || defined(TEENSYDUINO) || defined(ARDUINO_ARCH_ESP32) \
- || defined(ARDUINO_ARCH_ESP8266)
+ || defined(ARDUINO_ARCH_ESP8266) || defined(ARDUINO_ARCH_STM32)
 
 #ifdef LOW
 #undef LOW

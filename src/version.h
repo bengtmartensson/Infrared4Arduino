7fac29f4ba3951f4b536de7f1c5f706c802fa85c
Bengt Martensson <barf@bengt-martensson.de>
2021-02-10 21:01:08 +0100

    Set version to 1.2.0.

---
+++ src/version.h
@@ -0,0 +1,7 @@
+// This file was automatically generated from library.properties; do not edit.
+#pragma once
+/**
+ * Version of the current library.
+ * Taken from the version in library.properties.
+ */
+#define VERSION "1.2.0"

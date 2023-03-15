27d2fff0ed3faf49c01ccfbb6cb585ada825a939
Bengt Martensson <barf@bengt-martensson.de>
2023-03-15 13:56:28 +0100

    Fix memory leak in example. Resolves #89.

--- examples/Rc5Renderer/Rc5Renderer.ino
+++ examples/Rc5Renderer/Rc5Renderer.ino
@@ -5,16 +5,18 @@
 #include <IrSenderPwm.h>
 
 IrSender *sender;
+const IrSignal *signal_0;
+const IrSignal *signal_1;
 
 void setup() {
     sender = IrSenderPwm::getInstance(true);
+    signal_0 = Rc5Renderer::newIrSignal(0, 1); // toggle = 0
+    signal_1 = Rc5Renderer::newIrSignal(0, 1); // toggle = 1
 }
 
 void loop() {
-    const IrSignal *signal = Rc5Renderer::newIrSignal(0, 1);
-    sender->sendIrSignal(*signal);
+    sender->sendIrSignal(*signal_0);
     delay(1000);
-    signal = Rc5Renderer::newIrSignal(0, 1);
-    sender->sendIrSignal(*signal);
+    sender->sendIrSignal(*signal_1);
     delay(10000);
 }
